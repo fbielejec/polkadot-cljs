b859339c9351292138b3142fd529779165ee5361
filip <fbielejec@gmail.com>
2022-06-17 15:35:16 +0200

    readme udpate

--- README.md
+++ README.md
@@ -3,12 +3,29 @@
 ### deploy script
 
 ```bash
+cd scripts/deploy
 yarn deps
 yarn watch
-source env/dev
-node scripts/deploy.js
+source ../../env/dev
+yarn start
+```
+
+Get the REPL:
+
+```
 M-x cider-connect-clojurescript
 ```
 
 ### hex-to-wasm script
 
+```bash
+cd scripts/hex-to-wasm
+yarn deps
+yarn watch
+yarn start
+```
+
+Get the REPL:
+```
+M-x cider-connect-clojurescript
+```
--- src/hex_to_wasm/core.cljs
+++ src/hex_to_wasm/core.cljs
@@ -1,14 +1,23 @@
 (ns hex-to-wasm.core
-  (:require ["@polkadot/" :refer (CodePromise)]
-   [cljs.nodejs :as nodejs]))
+  (:require ["@polkadot/util" :refer (u8aToU8a)]
+            [shared.files :as files]
+            [cljs.nodejs :as nodejs]))
 
 (nodejs/enable-util-print!)
+
+;; TODO : CLI
 
 (defn start!
   "called by main and after reloading the code"
   []
   (prn "Starting")
-  )
+  (let [contract (files/read-json "/home/filip/CloudStation/aleph/aleph-node/contracts/button_token/target/ink/button_token.contract")
+        wasm (-> contract :source :wasm)
+        binary (u8aToU8a wasm)]
+
+    ;; (prn (u8aToU8a wasm))
+
+    ))
 
 (defn stop! "called before reloading code"
   []
