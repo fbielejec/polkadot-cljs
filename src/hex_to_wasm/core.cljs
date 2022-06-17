8bdc6a62a38b511dc5054bd9c879a9c8c4675b27
filip <fbielejec@gmail.com>
2022-06-17 16:39:49 +0200

    formatting

--- src/hex_to_wasm/core.cljs
+++ src/hex_to_wasm/core.cljs
@@ -12,17 +12,19 @@
   []
   (prn "Starting")
   (let [[from to] @arguments
-        _ (prn (str "Reading WASM in hex from: " from " and writing to " to))
-        contract (files/read-json from)
-        wasm (u8aToU8a (-> contract :source :wasm))]
+        _         (prn (str "Reading WASM in hex from: " from " and writing to " to))
+        contract  (files/read-json from)
+        wasm      (u8aToU8a (-> contract :source :wasm))]
     (assert (isWasm wasm) "not a WASM binary")
     (files/write-file (js/Buffer.from wasm) to)))
 
-(defn stop! "called before reloading code"
+(defn stop!
+  "called before reloading code"
   []
   (prn "Stopping"))
 
-(defn main "executed once, on startup, can do one time setup here"
+(defn main
+  "executed once, on startup, can do one time setup here"
   [& args]
   ;; NOTE: we write to atom just for development purposes, to keep args in between hot reloads
   (reset! arguments args)
