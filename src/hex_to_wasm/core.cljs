800c33b63aeb8a4444beba0438f30ef71bcf7df6
filip <fbielejec@gmail.com>
2022-06-17 16:35:58 +0200

    release scripts

--- src/hex_to_wasm/core.cljs
+++ src/hex_to_wasm/core.cljs
@@ -5,21 +5,25 @@
 
 (nodejs/enable-util-print!)
 
-;; TODO : CLI
+(defonce arguments (atom nil))
 
 (defn start!
   "called by main and after reloading the code"
   []
   (prn "Starting")
-  (let [contract (files/read-json "/home/filip/CloudStation/aleph/aleph-node/contracts/button_token/target/ink/button_token.contract")
+  (let [[from to] @arguments
+        _ (prn (str "Reading WASM in hex from: " from " and writing to " to))
+        contract (files/read-json from)
         wasm (u8aToU8a (-> contract :source :wasm))]
     (assert (isWasm wasm) "not a WASM binary")
-    (files/write-file (js/Buffer.from wasm) "/home/filip/CloudStation/aleph/aleph-node/contracts/button_token/target/ink/button_token.wasm")))
+    (files/write-file (js/Buffer.from wasm) to)))
 
 (defn stop! "called before reloading code"
   []
   (prn "Stopping"))
 
 (defn main "executed once, on startup, can do one time setup here"
-  []
+  [& args]
+  ;; NOTE: we write to atom just for development purposes, to keep args in between hot reloads
+  (reset! arguments args)
   (start!))
